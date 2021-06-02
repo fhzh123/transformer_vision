0a4913b40bfc440cae29ac6f301ae82d9b105268
herbwood <herbwood27@gmail.com>
2021-06-02 11:29:02 +0900

    updated transformer encoder layer

--- model/transformer/layer.py
+++ model/transformer/layer.py
@@ -25,10 +25,21 @@ class TransformerEncoderLayer(nn.Module):
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
 
+    # @autocast()
+    # def forward(self, src: Tensor, src_mask: Tensor = None, src_key_padding_mask: Tensor = None, pos=None) -> Tensor:
+    #     if pos is not None:
+    #         src = src + pos
+    #     src2 = self.self_attn(src, src, src, attn_mask=src_mask,
+    #                           key_padding_mask=src_key_padding_mask)[0]
+    #     src = src + self.dropout1(src2)
+    #     src = self.norm1(src)
+    #     src2 = self.linear2(self.dropout(F.gelu(self.linear1(src))))
+    #     src = src + self.dropout2(src2)
+    #     src = self.norm2(src)
+    #     return src
+
     @autocast()
-    def forward(self, src: Tensor, src_mask: Tensor = None, src_key_padding_mask: Tensor = None, pos=None) -> Tensor:
-        if pos is not None:
-            src = src + pos
+    def forward(self, src: Tensor, src_mask: Tensor = None, src_key_padding_mask: Tensor = None) -> Tensor:
         src2 = self.self_attn(src, src, src, attn_mask=src_mask,
                               key_padding_mask=src_key_padding_mask)[0]
         src = src + self.dropout1(src2)
